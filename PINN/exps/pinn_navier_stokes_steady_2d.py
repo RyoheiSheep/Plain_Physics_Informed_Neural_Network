c128f024cf14f2df38aaba3e4af603ccf3340f27
Ryohei <ryohei.sheep@gmail.com>
2024-04-17 23:24:44 +0900

    modified an exp file for better structure and loss function

--- PINN/exps/pinn_navier_stokes_steady_2d.py
+++ PINN/exps/pinn_navier_stokes_steady_2d.py
@@ -5,12 +5,12 @@ class Exp(MyExp):
     def __init__(self):
         super().__init__()
         self.input_size=2
-        self.hidden_layers=[4,4,4]
+        self.hidden_layers=[20,20,20]
         self.output_size=3
         
         self.Re=10
 
         self.exp_name=os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         
-        self.lambda_bd=1.0
-        self.lambda_res=5.0
+        self.lambda_bd=5.0
+        self.lambda_res=2.0
